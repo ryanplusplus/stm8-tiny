7819b697276e5adc9fa51d206b16db8319fae679
Ryan Hartlage <ryanplusplus@gmail.com>
2021-12-31 20:54:46 -0500

    Added interrupts_wait_for_interrupt()

--- src/interrupts.h
+++ src/interrupts.h
@@ -11,6 +11,7 @@
 
 #define interrupts_enable enableInterrupts
 #define interrupts_disable disableInterrupts
+#define interrupts_wait_for_interrupt wfi
 
 uint8_t interrupts_save(void);
 void interrupts_restore(uint8_t state);
