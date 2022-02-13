36457f35e40c9509e796295921d2881fb03924be
Ryan Hartlage <ryanplusplus@gmail.com>
2022-02-13 11:53:18 -0500

    Updated lib

--- lib_stm8-tiny.mk
+++ lib_stm8-tiny.mk
@@ -1,15 +1,11 @@
-__lib_stm8-tiny_path := $(subst $(shell pwd)/,,$(dir $(realpath $(lastword $(MAKEFILE_LIST)))))
+__lib_stm8-tiny_path := $(call current_path)
 
 include $(__lib_stm8-tiny_path)lib/tiny/lib_tiny.mk
 
-LIBS := stm8-tiny $(LIBS)
+$(call create_lib_with_defaults,stm8-tiny)
 
 INC_DIRS += \
   $(__lib_stm8-tiny_path)src \
-
-stm8-tiny_ASFLAGS := $(ASFLAGS)
-stm8-tiny_CPPFLAGS := $(CPPFLAGS)
-stm8-tiny_CFLAGS := $(CFLAGS)
 
 stm8-tiny_DEFINES := $(DEFINES)
 
